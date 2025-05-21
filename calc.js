ac9e6cf4221a3245c143fb7f6a615f0f974ce2da
Raksha Shetty <rshetty2406@gmail.com>
2025-05-21 10:12:49 +0530

    My calculator project

---
+++ calc.js
@@ -0,0 +1,14 @@
+function clrscr()
+{
+    document.getElementById("result").value="";
+}
+function display(value)
+{
+    document.getElementById("result").value+=value;
+}
+function calculate()
+{
+    var p=document.getElementById("result").value;
+    var q=eval(p);
+    document.getElementById("result").value=q;
+}
